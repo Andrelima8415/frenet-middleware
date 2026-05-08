b53b27e35efbe0b78c0e1c7984fec6e94519baf3
Andrelima8415 <andre@guday.com.br>
2026-05-08 16:11:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const express = require("express");
-const axios = require("axios");
-
 const app = express();
 app.use(express.json());
 
-const FRENET_TOKEN = process.env.FRENET_TOKEN;
-const SHOPIFY_TOKEN = process.env.SHOPIFY_TOKEN;
-const SHOPIFY_STORE = process.env.SHOPIFY_STORE;
-const FRENET_API_URL = "https://api.frenet.com.br/shipping/quote";
-const FUSO_HORARIO = "America/Sao_Paulo";
-const CORTE_HORA = 10;
-const DIAS_ANTES = 1;
-const DIAS_DEPOIS = 2;
+app.get("/", function(req, res) {
+  res.json({ status: "ok" });
+});
 
-function calcularBufferDias() {
-  const agora = new Date();
-  const horaLocal = new Intl.DateTimeFormat("pt-BR", {
-    timeZone: FUSO_HORARIO,
-    hour: "numeric",
-    hour12: false,
-  }).format(agora);
-  const hora = parseInt(horaLocal, 10);
-  re
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log("Porta " + PORT);
+});
